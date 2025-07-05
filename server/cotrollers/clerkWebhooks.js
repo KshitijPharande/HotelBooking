9e2934d786b3f00fab419ce325f22aa67aade13f
Reaper1737 <pharandekshitij@gmail.com>
2025-07-05 12:30:56 +0530

    Added Serverless Function

--- server/cotrollers/clerkWebhooks.js
+++ server/cotrollers/clerkWebhooks.js
@@ -1,62 +1,46 @@
-import User from '../models/User';
+import User from '../models/User.js';
 import { Webhook } from 'svix';
 
-const clerkWebhooks = async (req, res)=>{
+const clerkWebhooks = async (req, res) => {
     try {
-        // create Svix instance with clerk webhook scret
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        //getting header
         const headers = {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"],
         };
 
+        await whook.verify(JSON.stringify(req.body), headers);
 
-        // verifying headers
-            await whook.verify(JSON.stringify(req.body), headers)
+        const { data, type } = req.body;
 
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: `${data.first_name} ${data.last_name}`,
+            image: data.image_url,
+        };
 
-            // getting data from request body
+        switch (type) {
+            case "user.created":
+                await User.create(userData);
+                break;
+            case "user.updated":
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            case "user.deleted":
+                await User.findByIdAndDelete(data.id);
+                break;
+            default:
+                break;
+        }
 
-            const {data, type} = req.body
-
-            const userData = {
-                _id: data.id,
-                email: data.email_addresses[0].email_addresses,
-                username: data.first_name + " " + data.last_name,
-                image: data.image_url,
-                
-            };
-
-            //Switch case for different type 
-            switch (type) {
-                case "user.created":{
-                    await User.create(userData);
-                     break;
-                }
-                   case "user.updated":{
-                    await User.findbyIdAndUpdate(data.id, userData);
-                     break;
-                }    
-
-                   case "user.deleted":{
-                    await User.findbyIdAndDelete(data.id);
-                     break;
-                }  
-                   
-                default:
-                    break;
-            }
-            res.json({success: true, message:"Webhook received"})
-
-
+        res.status(200).json({ success: true, message: "Webhook received" });
     } catch (error) {
-        console.log(error.message)
-        res.json({success: false, message: error.message})
+        console.error(error.message);
+        res.status(500).json({ success: false, message: error.message });
     }
+};
 
-
-}
 export default clerkWebhooks;
